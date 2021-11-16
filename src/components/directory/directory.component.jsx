25c96e4c6a44a90c8ff7b74af15c5ab01b8a0933
pimby <baraka608@student.mmarau.ac.ke>
2021-11-16 14:26:50 +0300

    broken home page into smaller components finished styling of menu

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
+
+class Directory extends React.Component {
+    constructor (){
+        super();
+
+        this.state = {
+            sections: [
+                {
+                  title: 'hats',
+                  imageUrl: 'https://images.unsplash.com/photo-1504087263797-29091cad4826?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+                  id: 1,
+                  linkUrl: 'shop/hats'
+                },
+                {
+                  title: 'jackets',
+                  imageUrl: 'https://images.unsplash.com/photo-1559551409-dadc959f76b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1473&q=80',
+                  id: 2,
+                  linkUrl: 'shop/jackets'
+                },
+                {
+                  title: 'sneakers',
+                  imageUrl: 'https://images.unsplash.com/photo-1612902376491-7a8a99b424e8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80',
+                  id: 3,
+                  linkUrl: 'shop/sneakers'
+                },
+                {
+                  title: 'womens',
+                  imageUrl: 'https://images.unsplash.com/photo-1572804013427-4d7ca7268217?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=765&q=80',
+                  size: 'large',
+                  id: 4,
+                  linkUrl: 'shop/womens'
+                },
+                {
+                  title: 'mens',
+                  imageUrl: 'https://images.unsplash.com/photo-1593030761757-71fae45fa0e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
+                  size: 'large',
+                  id: 5,
+                  linkUrl: 'shop/mens'
+                }
+              ] 
+        }
+    }
+
+    render(){
+        return (
+            <div className = 'directory-menu' >
+                {
+                    this.state.sections.map(({title,imageUrl, size ,id}) => (
+                        <MenuItem key ={id} size = {size} title={title} imageUrl={imageUrl} />
+                    ))
+                }
+            </div>
+        )
+    }
+ 
+}
+
+export default Directory;
