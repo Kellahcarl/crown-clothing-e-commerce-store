0ce0e6933659271292899024d9e4a2632235697d
pimby <baraka608@student.mmarau.ac.ke>
2021-11-17 09:24:07 +0300

    update out menu-items to navigate withRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route } from 'react-router-dom';
+
+const TopicsList = () => ( 
+    <div >
+        <h1> topic list Page </h1> 
+    </div>
+)
+
+const TopicDetail = () => ( 
+    <div >
+        <h1> topic detail Page </h1> 
+    </div>
+)
 
 function App() {
-    return ( <
-        div >
-        <
-        HomePage / >
-        <
-        /div>
+    return ( 
+        <div>
+                <Route exact path='/' component={HomePage}/>
+                <Route exact path='/hats' component={TopicsList}/>
+                <Route path='/topics/:topicId' component={TopicDetail}/>  
+        </div>
     );
 }
 export default App;
