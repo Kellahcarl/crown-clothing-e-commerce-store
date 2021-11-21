8e8964df44229a62151f3c283fd298ca5e688f08
pimby <baraka608@student.mmarau.ac.ke>
2021-11-21 13:18:51 +0300

    added header and links to shop and home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
+import { Route , Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
-
-const TopicDetail = () => ( 
-    <div >
-        <h1> topic detail Page </h1> 
-    </div>
-)
 
 function App() {
     return ( 
         <div>
+            <Header />
+            <Switch>
                 <Route exact path='/' component={HomePage}/>
-                <Route exact path='/shop' component={ShopPage}/>  
+                <Route  path='/shop' component={ShopPage}/>  
+            </Switch>
         </div>
     );
 }
